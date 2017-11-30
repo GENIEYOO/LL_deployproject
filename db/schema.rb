ece385f23fd7ee344a117c75dcf9625a19d58769
GENIEYOO <forstudym.m@gmail.com>
2017-11-30 07:20:12 +0000

    fourth message

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130055056) do
+ActiveRecord::Schema.define(version: 20171130071854) do
 
   create_table "homes", force: :cascade do |t|
     t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
